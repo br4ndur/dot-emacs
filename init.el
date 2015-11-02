8d6060886d6d6243740691ea1a83735603e43128
Brandur Øssursson <branduren@gmail.com>
2015-11-02 01:12:20 +0100

    Set font face

--- init.el
+++ init.el
@@ -3,25 +3,3 @@
 (require 'cl)
 (org-babel-load-file "~/.emacs.d/branduren.org")
 (put 'dired-find-alternate-file 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ac-ispell-fuzzy-limit 4)
- '(ac-ispell-requires 4)
- '(paradox-github-token t t)
- '(send-mail-function (quote smtpmail-send-it)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(erc-input-face ((t (:foreground "antique white"))))
- '(helm-selection ((t (:background "ForestGreen" :foreground "black"))))
- '(org-agenda-clocking ((t (:inherit secondary-selection :foreground "black"))) t)
- '(org-agenda-done ((t (:foreground "dim gray" :strike-through nil))))
- '(org-clock-overlay ((t (:background "SkyBlue4" :foreground "black"))))
- '(org-done ((t (:foreground "PaleGreen" :weight normal :strike-through t))))
- '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:foreground "LightSalmon" :strike-through t))))
- '(outline-1 ((t (:inherit font-lock-function-name-face :foreground "cornflower blue")))))
